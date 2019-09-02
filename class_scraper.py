8b54c1597d906e0e73a8191353d063a1361a04f8
Nicolas Shak <Nshak@Ucsc.edu>
2019-09-02 15:21:27 +0200

    save point

--- class_scraper.py
+++ class_scraper.py
@@ -4,11 +4,17 @@ import requests
 import json
 import pprint
 
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
 CLASS_API_ENDPOINT = 'https://pisa.ucsc.edu/class_search/'
 
 payload = {
 	'action': 'results',
-	'binds[:term]': '2192',
+	'binds[:term]': '2198',
 	'binds[:reg_status]': 'all',
 	'binds[:subject]': '',
 	'binds[:catalog_nbr_op]': '=', 
@@ -24,145 +30,101 @@ payload = {
 	'binds[:days]': '',
 	'binds[:times]': '',
 	'binds[:acad_career]': '',
-	'rec_dur': 346
+	'rec_dur': 10
 }
 
-aliases = {
-    'AMS': 'Math',
-    'ANTH': 'Anthropology',
-    'APLX': 'Linguistics',
-    'ART': 'Art',
-    'ARTG': 'Games and Playable Media',
-    'ASTR': 'Astronomy',
-    'BIOE': 'Biology',
-    'BIOL': 'Biology',
-    'BME': 'Business',
-    'CHEM': 'Chemistry',
-    'CHIN': 'Language',
-    'CLNI': 'College Affiliated',
-    'CLTE': 'College Affiliated',
-    'CMMU': 'Community Studies',
-    'CMPE': 'Technology',
-    'CMPM': 'Technology',
-    'CMPS': 'Technology',
-    'COWL': 'College Affiliated',
-    'CRES': 'Critical Race and Ethnic Studies',
-    'CRSN': 'College Affiliated',
-    'CRWN': 'College Affiliated',
-    'EART': 'Earth and Planetary Sciences',
-    'ECON': 'Economics',
-    'EDUC': 'Education',
-    'EE': 'Electrical Engineering',
-    'ENVS': 'Environmental Studies',
-    'ESCI': 'Environmental Science',
-    'FILM': 'Film and Digital Media',
-    'FMST': 'Feminist Studies',
-    'FREN': 'Language',
-    'GERM': 'Language',
-    'HAVC': 'History of Art and Visual Culture',
-    'HEBR': 'Language',
-    'HIS': 'History',
-    'HISC': 'History of Consciousness',
-    'ITAL': 'Language',
-    'JAPN': 'Language',
-    'KRSG': 'College Affiliated',
-    'LALS': 'Latin American and Latino Studies',
-    'LGST': 'Legal Studies',
-    'LING': 'Linguistics',
-    'LIT': 'Literature',
-    'MATH': 'Math',
-    'MERR': 'College Affiliated',
-    'METX': 'Microbiology and Environmental Toxicology',
-    'MUSC': 'Music',
-    'OAKS': 'College Affiliated',
-    'OCEA': 'Ocean Sciences',
-    'PHIL': 'Philosophy',
-    'PHYS': 'Physics',
-    'POLI': 'Politics',
-    'PORT': 'College Affiliated',
-    'PRTR': 'College Affiliated',
-    'PSYC': 'Psychology',
-    'SOCY': 'Sociology',
-    'SPAN': 'Language',
-    'SPHS': 'Language',
-    'STEV': 'College Affiliated',
-    'THEA': 'Theater Arts',
-    'WRIT': 'Writing'
-}
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+
+SPREADSHEET_ID = '1cF9wFa9xuMATQUPUqKrkoUDTywZXqhijWaEuHywIu0w'
 
 def main():
 
-	class_list = scrape(2192)
+	class_list = scrape(2198)
 
-# Returns json of all classes and class information
-def scrape(term):
+	sheet = getSheetsInstance()
 
-	pp = pprint.PrettyPrinter(indent=4)
+	add_sheet_obj = {
+	    'requests': [
+	        {
+	            'addSheet': {
+	                'properties': {
+	                    'title': quarter
+	                }
+	            }
+	        }
+	    ]
+	}
+
+	sheet.batchUpdate(spreadsheetId=SPREADSHEET_ID, body=add_sheet_obj).execute()
+
+	class_list.insert(0, ['Course Title', 'Description', 'Prerequisites', 'Instructor', 'GE', 'Link'])
+	class_obj = {
+		'valueInputOption': 'USER_ENTERED',
+		'data': [
+			{
+				'range': quarter + '!A:F',
+				'majorDimension': 'ROWS',
+				'values': class_list
+			}
+		],
+		'includeValuesInResponse': False
+	}
+
+	sheet.values().batchUpdate(spreadsheetId=SPREADSHEET_ID, body=class_obj).execute()
+
+# Returns tuple with scraped quarter and list of lists of each class' information
+def scrape(term):	
 
 	request = requests.post(CLASS_API_ENDPOINT, data=payload)
 
 	class_list = BeautifulSoup(request.text, 'html.parser')
 	classes = class_list.find_all(id=re.compile('class_nbr'))
 
-	class_info_list = {}
+	class_info_list = []
 
 	for class_element in classes:
 
-		class_info = {
-			'title': None,
-			'department': None,
-			'ge': None,
-			'description': None,
-			'instructors': None,
-			'link': None
-		}
+		class_info = []
 
 		request2 = requests.get(class_element['href'])
 		class_page = BeautifulSoup(request2.text, 'html.parser')
 
-		class_info['link'] = class_element['href']
-
 		# Class title / department
 		class_title = class_page.find('h2')
 
-		if class_title:
+		print('*****', class_title.get_text().strip(), '*****')
+		class_info.append(class_title.get_text().strip().replace('\u00a0\u00a0', ''))
 
-			print('*****', class_title.get_text().strip(), '*****')
-			class_info['title'] = class_title.get_text().strip().replace('\u00a0\u00a0', '')
-
-			class_info['department'] = aliases[class_info['title'].split(' ', 1)[0]]
-
-		# GE
-		details = class_page.find_all('dd')
-
-		if details:
-
-			class_info['ge'] = details[5].get_text().strip()
+		#class_info['department'] = aliases[class_info['title'].split(' ', 1)[0]]
 
 		# Description
 		description = parse_panel(class_page, 'Description')
+		class_info.append(description.get_text().strip())
 
 		# Enrollment requirements
 		requirements = parse_panel(class_page, 'Enrollment Requirements')
 		if requirements:
-			class_info['enrollment_requirements'] = requirements.get_text().strip()
-
-		if description:
-
-			class_info['description'] = description.get_text().strip()
+			class_info.append(requirements.get_text().strip())
+		else:
+			class_info.append('Prerequisite(s): N/A')
 
 		# Instructor
 		meeting_info = parse_list(class_page, 'Meeting Information', 'td')
-		
 		if meeting_info:
+			class_info.append(meeting_info[2])
+		else:
+			class_info.append('N/A')
 
-			class_info['instructors'] = meeting_info[2]
+		# GE
+		details = class_page.find_all('dd')
+		class_info.append(details[5].get_text().strip())
 
-		class_info_list[class_info['title']] = class_info
+		# Link
+		class_info.append(class_element['href'])
 
-	#pp.pprint(class_info_list)
-	with open('classes.json', 'w') as fp:
-		json.dump(class_info_list, fp, sort_keys=True)
+		class_info_list.append(class_info)
+
+	return class_info_list
 
 # Returns list with information in order found
 def parse_list(page, heading, tag, **kargs):
@@ -184,6 +146,33 @@ def parse_panel(page, heading):
 	else:
 		print(heading, 'not found')
 		return None
+
+# Handles credentials and token if not found (from GSheets API docs)
+def getSheetsInstance():
+
+	creds = None
+	# The file token.pickle stores the user's access and refresh tokens, and is
+	# created automatically when the authorization flow completes for the first
+	# time.
+	if os.path.exists('token.pickle'):
+		with open('token.pickle', 'rb') as token:
+			creds = pickle.load(token)
+	# If there are no (valid) credentials available, let the user log in.
+	if not creds or not creds.valid:
+		if creds and creds.expired and creds.refresh_token:
+			creds.refresh(Request())
+		else:
+			flow = InstalledAppFlow.from_client_secrets_file(
+				'credentials.json', SCOPES)
+			creds = flow.run_local_server(port=0)
+		# Save the credentials for the next run
+		with open('token.pickle', 'wb') as token:
+			pickle.dump(creds, token)
+
+	service = build('sheets', 'v4', credentials=creds)
+
+	# Call the Sheets API
+	return service.spreadsheets()
 
 if __name__ == '__main__':
 	main()
